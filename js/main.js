285a376e510d87cbddb2633491bffdb6de8e0284
Erik Bolton <ksu.deadeye@gmail.com>
2020-07-23 11:01:02 -0700

    working menus

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ const terms = {};
 const subjects = {};
 const courses = {};
 
+let courseList = [];
 let subjectKeys = [];
 
 // set the dimensions and margins of the graph
@@ -59,25 +60,28 @@ const grid = d3.select("#grid")
 const cellsPassive = d3.select("#cellsPassive")
   .attr("transform", "translate(" + margin.left + "," + xLabelHeight + ")");
 
-const cellsActive = d3.select("#cellsActive")
-  .attr("transform", "translate(" + margin.left + "," + xLabelHeight + ")");
-
 const mainBody = d3.select("#mainBody")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+const cellsActive = d3.select("#cellsActive")
+  .attr("transform", "translate(" + margin.left + "," + xLabelHeight + ")");
 
 const labels = d3.select("#labels")
   .attr("transform", "translate(" + 0 + "," + 0 + ")");
 
+const circles = d3.select("#mainBody").select("#circles");
+const menu = d3.select("#mainBody").select("#menu");
 const debugText = d3.select("#debug");
+
+let currentMenuSelection = "";
 
 classLevels.forEach(function(level, index) {
   grid.append("line")
+    .attr("class", "xLine")
     .attr("x1", x(level))
     .attr("y1", -maxRadius)
     .attr("x2", x(level))
-    .attr("y2", height)
-    .attr("stroke", "rgb(255, 0, 0)")
-    .attr("stroke-width", 2)
+    .attr("y2", height);
 });
 
 d3.csv(subjectDataFile).then(function(data) {
@@ -92,66 +96,118 @@ d3.csv(subjectDataFile).then(function(data) {
   subjectKeys.forEach(function(subject, i) {
     classLevels.forEach(function (level, j) {
       cellsPassive.append("rect")
+        .attr("id", subject + level.toString())
+        .attr("data-subject", subject)
+        .attr("data-level", level)
+        .attr("class", "cell passive out")
         .attr("x", x(level))
         .attr("y", y(subject))
         .attr("width", cellWidth)
-        .attr("height", cellHeight)
-        .attr("fill", "rgb(0, 0, 255)")
-        .attr("opacity", 0.40);
+        .attr("height", cellHeight);
 
       cellsActive.append("rect")
+        .attr("id", subject + level.toString())
+        .attr("class", "cell active")
         .attr("x", x(level))
         .attr("y", y(subject))
         .attr("width", cellWidth)
         .attr("height", cellHeight)
-        .attr("opacity", 0.0)
         .on("mouseover", handleMouseOver)
-        .on("mouseout", handleMouseOut);
+        .on("mouseout", handleMouseOut)
+        .on("click", handleClick);
     });
   });
 
-  labels.selectAll(".ylabel")
+  labels.selectAll(".yLabel")
     .data(data)
-    .enter()
-    .append("text")
+    .enter().append("text")
       .text(function(d) { return d["Subject"]; })
-      .attr("class", "subject")
-      .attr("text-anchor", "end")
-      .attr("alignment-baseline", "middle")
+      .attr("class", "yLabel subject")
       .attr("x", yLabelWidth - yLabelPad)
       .attr("y", function(d) { return y(d["Subject"]) + margin.top; });
 
-  grid.selectAll(".yline")
+  grid.selectAll(".yLine")
     .data(data)
-    .enter()
-    .append("line")
+    .enter().append("line")
+      .attr("class", "yLine")
       .attr("x1", -maxRadius)
       .attr("y1", function(d) { return y(d["Subject"]) })
-      .attr("x2", width)
+      .attr("x2", width + maxRadius)
       .attr("y2", function(d) { return y(d["Subject"]) })
-      .attr("stroke", "rgb(255, 0, 0)")
-      .attr("stroke-width", 1)
-      .attr("opacity", 0.25);
 });
 
+let testList = [];
 let testString = "";
+let testObject = Object.create(null);
 let testInt = 0;
 let maxTotal = 0;
 
-function handleMouseOver(d, i) {
-  console.log("over d:" + d + ", i:" + i);
-  d.attr({
-    fill: "orange",
-    stroke: "black"
-  });
+function handleMouseOver() {
+  let cellPassive = d3.select("g#cellsPassive").select("rect#" + this.id + ".cell.passive");
+  if (!cellPassive.attr("class").includes("click")) {
+    cellPassive.attr("class", "cell passive over");
+  }
 }
 
-function handleMouseOut(d, i) {
-  console.log("out d:" + d + ", i:" + i);
-  d.attr({
-    fill: "black",
-    stroke: "none"
-  });
+function handleMouseOut() {
+  let cellPassive = d3.select("g#cellsPassive").select("rect#" + this.id + ".cell.passive");
+  if (!cellPassive.attr("class").includes("click")) {
+    cellPassive.attr("class", "cell passive out");
+  }
+}
+
+function handleClick() {
+  if (currentMenuSelection && (currentMenuSelection === this.id)) return;
+
+  if (currentMenuSelection !== "") {
+    let currentCellPassive = d3.select("g#cellsPassive").select("rect#" + currentMenuSelection + ".cell.passive");
+    currentCellPassive.attr("class", "cell passive");
+  }
+
+  let cellPassive = d3.select("g#cellsPassive").select("rect#" + this.id + ".cell.passive");
+  cellPassive.attr("class", "cell passive click");
+
+  let subject = cellPassive.attr("data-subject");
+  let level = parseInt(cellPassive.attr("data-level"));
+  let x = parseInt(cellPassive.attr("x"));
+  let y = parseInt(cellPassive.attr("y")) + parseInt(cellPassive.attr("height")) / 2.0;
+  let itemHeight = 22;
+  let itemWidth = parseInt(cellPassive.attr("width"));
+
+  menu.attr("transform", "translate(" + x + "," + y + ")");
+  menu.selectAll(".menuItem").remove();
+
+  let menuSelect = menu.selectAll(".menuItem")
+    .data(courseList)
+    .enter()
+    .filter(function(d) { return d["Subject"] === subject })
+    .filter(function(d) { return (d["Number"] >= level) && (d["Number"] <= level + 99) })
+    .append("g")
+      .attr("class", "menuItem");
+
+  menuSelect.append("rect")
+    .attr("class", "menuItemRect")
+    .attr("x", 0)
+    .attr("y", function(d, i) { return i * itemHeight })
+    .attr("width", itemWidth)
+    .attr("height", itemHeight);
+
+  menuSelect.append("text")
+    .text(menuText)
+    .attr("class", "menuItemText")
+    .attr("x", 0)
+    .attr("y", function(d, i) { return i * itemHeight + itemHeight / 2.0 });
+
+  debugText.selectAll(".debug")
+    .data(courseList)
+    .enter()
+    .filter(function(d) { return d["Subject"] === subject })
+    .filter(function(d) { return (d["Number"] >= level) && (d["Number"] <= level + 99) })
+    .append("p")
+      .attr("class", "debug")
+      .text(writeDebug);
+
+  currentMenuSelection = this.id;
 }
 
 d3.csv(gpaDataFile).then(function(data) {
@@ -181,38 +237,37 @@ d3.csv(gpaDataFile).then(function(data) {
     }
   });
 
-  let courseList = Object.values(courses);
+  courseList = Object.values(courses);
 
   // set subject scale
   r.domain([0, maxTotal]);
 
-  // append the circles for the chart
-  mainBody.selectAll(".circle")
+  // add the circles to the chart
+  circles.selectAll(".course")
     .data(courseList)
-    .enter().append("circle")
+    .enter()
     .filter(function(d) { return d["Subject"] in subjects })
-    .attr("cx", function(d) { return x(parseInt(d["Number"])); })
-    .attr("cy", function(d) { return y(d["Subject"]); })
-    .attr("r", function(d) { return r(d["Total"]); })
-    .attr("fill-opacity", .25);
-
-  // render debug text
-  debugText.selectAll(".debug")
-    .data(courseList)
-    .enter().append("p")
-    .filter(function(d) { return d["Subject"] in subjects })
-    .filter(function(d) { return (d["Number"] >= 498) && (d["Number"] <= 498) })
-    .text(function(d) {
-      let propValue = "";
-      let propString = "";
-      for(let propName in d) {
-        propValue = d[propName]
-        propString += propName + " - " + propValue + " "
-      }
-      propString += " \n"
-      return propString;
-    })
+    .append("circle")
+      .attr("class", "course")
+      .attr("cx", function(d) { return x(parseInt(d["Number"])); })
+      .attr("cy", function(d) { return y(d["Subject"]); })
+      .attr("r", function(d) { return r(d["Total"]); });
 });
+
+function menuText(d) {
+  return d["Number"] + "-" + d["Title"]
+}
+
+function writeDebug(d) {
+  let propValue = "";
+  let propString = "";
+  for(let propName in d) {
+    propValue = d[propName]
+    propString += propName + " - " + propValue + " "
+  }
+  propString += " \n"
+  return propString;
+}
 
 // get the data
 /*
